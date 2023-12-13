af5a06543b09fa56405fe77c3fd3c940cc91a162
Eric Hovhannisyan <Erichovhannisyan13@gmail.com>
2023-12-13 21:35:02 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "Refresher",
     platforms: [
         .iOS(.v14),
-        .visionOS(.v1)
+        .visionOS(.v1),
+        .macOS(.v10_15),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
